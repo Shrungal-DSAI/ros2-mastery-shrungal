3df0df440e7eec09729d8d95cf460898237e5b94
Shrungal <shrungalkulkarni30@gmail.com>
2025-11-30 17:04:42 +0100

    Day4: add pytest tests, PEP8 fixes and GitHub Actions CI workflow

--- talker_listener/test/test_format_message.py
+++ talker_listener/test/test_format_message.py
@@ -1,9 +1,12 @@
 import pytest
+
 from talker_listener.talker import format_message
+
 
 def test_format_message_basic():
     assert format_message("hello SSK", 0) == "hello SSK 0"
     assert format_message("prefix", 42) == "prefix 42"
+
 
 @pytest.mark.parametrize("prefix,idx,expected", [
     ("a", 1, "a 1"),
